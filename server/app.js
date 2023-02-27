2ee3fa241ff699ac325114262c6e8fa263e7382c
rachel-eckert <110422541+rachel-eckert@users.noreply.github.com>
2023-02-27 16:30:17 -0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
-const volleyball = require('volleyball')
+
 const app = express()
 
 // static middleware
